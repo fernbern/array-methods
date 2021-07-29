e31534b66afc566ca66073082e29a63c685766f4
fernbern <70728008+fernbern@users.noreply.github.com>
2021-07-29 16:25:17 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 // 30 Array Methods in JavaScript
 concat();
-concat(); concat(); map();
+concat(); concat(); map(); map(); map();
 copyWithin(); map(); map();
 entries(); console.log() console.log(window.this);
 entries(); entries(); map(); map(); map(); map(); map();
